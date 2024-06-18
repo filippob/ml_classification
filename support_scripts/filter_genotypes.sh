df3d5c77702265e5b96f8dfe3d5f1b83ed443f96
filippob <filippo.biscarini@gmail.com>
2024-06-18 15:58:39 +0200

    added: fam filter for samples

--- support_scripts/filter_genotypes.sh
+++ support_scripts/filter_genotypes.sh
@@ -3,11 +3,11 @@
 ## GLOBAL PARAMS (FROM CONFIG FILE)
 source config.sh
 
-## SOFTWARE & FILES
+## LOCAL PARAMS
 inputfile='PHENOMENA_28122022'
 outdir='Analysis'
+keepf="keep.fam"
 
-## PARAMETERS
 MAC=100
 MIND=0.05
 
@@ -17,6 +17,6 @@ echo " - creating output directory"
 mkdir -p $outdir
 
 echo " - filtering genotype data ... "
-$plink --file $inputfile --allow-extra-chr --chr 1-23 --mac $MAC --mind $MIND --geno 0.01 --bp-space 1 --make-bed --out ${outdir}/filtered
+$plink --file $inputfile --allow-extra-chr --chr 1-23 --mac $MAC --mind $MIND --geno 0.01 --bp-space 1 --keep $keepf --make-bed --out ${outdir}/filtered
 
 echo "DONE!"
