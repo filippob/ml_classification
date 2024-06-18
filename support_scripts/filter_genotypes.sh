00679ea65f6986f9c71b1b064b60df5aa333f223
filippob <filippo.biscarini@gmail.com>
2024-06-18 14:38:40 +0200

    added: output directory

--- support_scripts/filter_genotypes.sh
+++ support_scripts/filter_genotypes.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+## SOFTWARE & FILES
 plink=/home/filippo/Downloads/plink
 inputfile='PHENOMENA_28122022'
+outdir='Analysis'
+
+## PARAMETERS
 MAC=100
 MIND=0.05
 
+echo " - creating output directory"
+mkdir -p $outdir
+
 echo " - filtering genotype data ... "
-$plink --file $inputfile --allow-extra-chr --chr 1-23 --mac $MAC --mind $MIND --geno 0.01 --bp-space 0 --recode --out filtered
+$plink --file $inputfile --allow-extra-chr --chr 1-23 --mac $MAC --mind $MIND --geno 0.01 --bp-space 0 --recode --out ${outdir}/filtered
 
 echo "DONE!"
