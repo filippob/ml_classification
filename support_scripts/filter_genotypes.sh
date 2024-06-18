a8edb2f5eed1b9eaa79600824d39d7b655811b80
filippob <filippo.biscarini@gmail.com>
2024-06-18 15:18:54 +0200

    fixed typos

--- support_scripts/filter_genotypes.sh
+++ support_scripts/filter_genotypes.sh
@@ -11,14 +11,12 @@ outdir='Analysis'
 MAC=100
 MIND=0.05
 
-echo $plink
-echo $beagle
-echo $basefolder
+echo "path to Plink is: $plink"
 
 echo " - creating output directory"
 mkdir -p $outdir
 
 echo " - filtering genotype data ... "
-#$plink --file $inputfile --allow-extra-chr --chr 1-23 --mac $MAC --mind $MIND --geno 0.01 --bp-space 1 --make-bed --out ${outdir}/filtered
+$plink --file $inputfile --allow-extra-chr --chr 1-23 --mac $MAC --mind $MIND --geno 0.01 --bp-space 1 --make-bed --out ${outdir}/filtered
 
 echo "DONE!"
