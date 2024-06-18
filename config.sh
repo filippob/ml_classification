4df4452fc1356d0ac7429acf1c5fa2e87a143450
filippob <filippo.biscarini@gmail.com>
2024-06-18 15:17:42 +0200

    added: config file

---
+++ config.sh
@@ -0,0 +1,5 @@
+## GLOBAL PARAMETERS
+basefolder=/home/filippo/Documents/tania/PI
+plink=/home/filippo/Downloads/plink
+beagle=/home/filippo/Documents/software/beagle5/beagle.28Sep18.793.jar
+
--- support_scripts/filter_genotypes.sh
+++ support_scripts/filter_genotypes.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+## GLOBAL PARAMS (FROM CONFIG FILE)
+source config.sh
+
 ## SOFTWARE & FILES
-plink=/home/filippo/Downloads/plink
 inputfile='PHENOMENA_28122022'
 outdir='Analysis'
 
@@ -9,10 +11,14 @@ outdir='Analysis'
 MAC=100
 MIND=0.05
 
+echo $plink
+echo $beagle
+echo $basefolder
+
 echo " - creating output directory"
 mkdir -p $outdir
 
 echo " - filtering genotype data ... "
-$plink --file $inputfile --allow-extra-chr --chr 1-23 --mac $MAC --mind $MIND --geno 0.01 --bp-space 1 --make-bed --out ${outdir}/filtered
+#$plink --file $inputfile --allow-extra-chr --chr 1-23 --mac $MAC --mind $MIND --geno 0.01 --bp-space 1 --make-bed --out ${outdir}/filtered
 
 echo "DONE!"
