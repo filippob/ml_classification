5f99264be0f7c6ce37d772a5f9d7b0dd7a063f80
filippob <filippo.biscarini@gmail.com>
2024-07-02 17:13:03 +0200

    updated: filtering criteria as in Minnai et al.

--- support_scripts/filter_genotypes.sh
+++ support_scripts/filter_genotypes.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
 ## GLOBAL PARAMS (FROM CONFIG FILE)
-source config.sh
+source config.sh ## basefolder and paths to software from here
 
 ## LOCAL PARAMS
-inputfile='PHENOMENA_28122022'
-outdir='Analysis'
-keepf="keep.fam"
+inputfile='Analysis/1.cleaning/pi_cleaned'
+outdir='Analysis/2.filtering'
+keepf="data/keep.fam"
 
-MAC=100
-MIND=0.05
+MAF=0.01
+GENO=0.01
+HWE=1e-06
 
 echo "path to Plink is: $plink"
 
 echo " - creating output directory"
-mkdir -p $outdir
+mkdir -p $basefolder/$outdir
 
 echo " - filtering genotype data ... "
-$plink --file $inputfile --allow-extra-chr --chr 1-23 --mac $MAC --mind $MIND --geno 0.01 --bp-space 1 --snps-only 'just-acgt' --keep $keepf --make-bed --out ${outdir}/filtered
+$plink --file $basefolder/$inputfile --allow-extra-chr --chr 1-22 --geno $GENO --maf $MAF --hwe $HWE --bp-space 1 --snps-only 'just-acgt' --keep $keepf --make-bed --out ${outdir}/pi_filtered
 
 echo "DONE!"
