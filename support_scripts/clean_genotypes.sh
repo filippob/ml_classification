9f78858bbe463e4183348479b0f9188c40bb5806
filippob <filippo.biscarini@gmail.com>
2024-07-02 17:01:39 +0200

    fixed: var name

--- support_scripts/clean_genotypes.sh
+++ support_scripts/clean_genotypes.sh
@@ -8,7 +8,6 @@ source config.sh ## basefolder from here
 inputfile='data/genotyped_nofilters/PHENOMENA_28122022.ped'
 outdir='Analysis/1.cleaning'
 
-
 ## Rimuovi i controlli (HG00264 & HG00097) con record duplicati nei genotipi (assenti nel dataset dei fenotipi) per poter usare plink: 5 record con family ID 0 e stesso individual ID (HG00264 o HG00097) e 24 record ciascuno HG00XXX_n. (da 1 a 24) con individual e family ID uguali
 
 fname=$basefolder/$inputfile
@@ -17,11 +16,11 @@ echo "N. of samples in the original file"
 wc -l $fname
 
 #grep 'HG00264' genotyped_nofilters/PHENOMENA_28122022.ped | less -S
-grep -v 'HG00264' $fname > temp.ped
+grep -v 'HG00264' $fname > $outdir/temp.ped
 #grep 'HG00097' temp.ped | less -S
 
 outfile="$basefolder/$outdir/pi_cleaned.ped"
-grep -v 'HG00097' temp.ped > $outfile
+grep -v 'HG00097' $outdir/temp.ped > $outfile
 
 echo "N. of samples after cleaning"
 wc -l $outfile
@@ -31,5 +30,7 @@ basename="${inputfile%.*}"
 mapfile="$basefolder/$basename.map"
 
 cp $mapfile "$basefolder/$outdir/pi_cleaned.map"
+
+rm $outdir/temp.ped
 
 echo "DONE!"
