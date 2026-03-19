685c59606fbe69984fda48859bc00a5bf9bf412a
filippob <filippo.biscarini@gmail.com>
2026-03-19 11:40:17 +0100

    added: controls for case when there are no labels (real test/production data)

--- support_scripts/make_test.R
+++ support_scripts/make_test.R
@@ -7,11 +7,11 @@ library("data.table")
 
 ## Parameters
 basefolder <- "/home/filippo/Documents/tania/probiotics"
-input_file <- "data/preliminary_data.xlsx"
+input_file <- "data/filtered_merged_bits26_testset.csv"
 outdir = "splits"
-target_var = "Label"
-nclass_minority = 4
-nclass_majority = 30
+target_var = NULL
+nclass_minority = NULL
+nclass_majority = NULL
 to_remove = c("Assembly", "Class", "Locus_ID", "Version") ## columns to remove
 
 
@@ -28,8 +28,14 @@ print(paste("Data size (n. records):",nrow(dataset)))
 
 ## Data preparation
 ## making target variable a factor
-dataset <- dataset %>% 
-  mutate({{target_var}} := as.factor(.data[[target_var]]))
+
+if (!is.null(target_var)) {
+  
+  print("making the target variable a factor")
+  dataset <- dataset %>% 
+    mutate({{target_var}} := as.factor(.data[[target_var]]))
+}
+
 
 ## removing unnecessary columns
 dataset <- dataset |> select(!all_of(to_remove))
@@ -49,40 +55,48 @@ if(sum(is.na(dataset)) == 0) {
 # x <- dataset[vec,]
 ##################################
 
-## potential duplicate
-"Bifidobacterium_longum_subsp._infantis_ATCC_15697_=_JCM_1222_=_DSM"
-
 ## remove missing
 dataset <- na.omit(dataset) ## since we have two probiotics with missing data (potential duplicates): TO  CHECK !!!
-dataset |>
-  group_by(Label) |>
-  summarise(N = n())
+if (!is.null(target_var)) {
+  dataset |>
+    group_by(Label) |>
+    summarise(N = n())
+}
 
 ## training/test split
 writeLines(" - splitting the data in training and test sets")
 
-print(paste("N. of minority class records to be sampled for test:", nclass_minority))
-print(paste("N. of majority class records to be sampled for test:", nclass_majority))
+if (!is.null(nclass_minority) & !is.null(nclass_majority)) {
+  
+  print(paste("N. of minority class records to be sampled for test:", nclass_minority))
+  print(paste("N. of majority class records to be sampled for test:", nclass_majority))
+  
+  probiotics <- subset(dataset, Label == "Probiotic")
+  nonprobiotics <- subset(dataset, Label == "Nonprobiotic")
+  
+  vec <- sample(1:nrow(probiotics), nclass_minority)
+  test_probs = probiotics[vec,]
+  train_probs = probiotics[-vec,]
+  
+  vec <- sample(1:nrow(nonprobiotics), nclass_majority)
+  test_nonprobs = nonprobiotics[vec,]
+  train_nonprobs = nonprobiotics[-vec,]
+  
+  test <- test_probs |> bind_rows(test_nonprobs)
+  train <- train_probs |> bind_rows(train_nonprobs)
+  
+  writeLines(" - writing out test and training sets")
+  fname = file.path(basefolder, outdir, "test_set.csv")
+  fwrite(x = test, file = fname)
+  
+  fname = file.path(basefolder, outdir, "train_set.csv")
+  fwrite(x = train, file = fname)
+} else {
+  
+  writeLines(" - writing out independent test set")
+  fname = file.path(basefolder, outdir, basename(input_file))
+  fwrite(x = dataset, file = fname)
+}
 
-probiotics <- subset(dataset, Label == "Probiotic")
-nonprobiotics <- subset(dataset, Label == "Nonprobiotic")
-
-vec <- sample(1:nrow(probiotics), nclass_minority)
-test_probs = probiotics[vec,]
-train_probs = probiotics[-vec,]
-
-vec <- sample(1:nrow(nonprobiotics), nclass_majority)
-test_nonprobs = nonprobiotics[vec,]
-train_nonprobs = nonprobiotics[-vec,]
-
-test <- test_probs |> bind_rows(test_nonprobs)
-train <- train_probs |> bind_rows(train_nonprobs)
-
-writeLines(" - writing out test and training sets")
-fname = file.path(basefolder, outdir, "test_set.csv")
-fwrite(x = test, file = fname)
-
-fname = file.path(basefolder, outdir, "train_set.csv")
-fwrite(x = train, file = fname)
 
 print("DONE!!")
